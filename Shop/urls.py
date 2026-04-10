c6297afeb19f65359ac5b9accb422c93d4322e42
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-04-10 10:48:06 +0530

    Today's Updates 10/04/2026.

--- Shop/urls.py
+++ Shop/urls.py
@@ -5,11 +5,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="indec"),
+    path('', views.index, name="index"),
     path('products/', views.products, name="product"),
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
     path('catalogue/', views.catalogue, name="catalogue"),
     path('signup/', views.signupView, name="signup"),
     path('login/', views.loginView, name="login"),
+    path('logout/', views.logoutView, name="logout"),
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from .models import Contact
 from django.contrib.auth.models import User
@@ -27,23 +28,32 @@ def catalogue(r):
     return render(r, 'catalogue.html')
 
 def signupView(r):
-    if request.method == "POST":
-
+    if r.method == "POST":
         m5 = r.POST.get('emailAddress')
         m6 = r.POST.get('username')
         m7 = r.POST.get('password')
-        o = Contact(emailAddress=m5, username=m6, password=m7)
-        o.save()
-        return redirect('/signup/')
-        if password==cpassword:
-            user = User.objects.create_user(emailAddress, username, password)
-            user.first_name = firstName
-            user.last_name = lastName
-            user.save()
-            return redirect('/signup/')
+
+        if m6 and m7:
+            u = User.objects.create_user(username=m6, email=m5, password=m7)
+            u.save()
+            return redirect('/login/')
+        else:
+            return render(r, 'signup.html', {"error": "Valid data required"})
+
     return render(r, 'signup.html')
 
 def loginView(r):
-    if request.method == "POST":
-
+    if r.method == "POST":
+        u = r.POST.get('userName')
+        p = r.POST.get('password')
+        v = authenticate(username=u, password=p)
+        if v is not None:
+            login(r, v)
+            return redirect('/')
+        else:
+            return render(r, "login.html", {"error":"Username or Password is incorrect."})
     return render(r, 'login.html')
+
+def logoutView(r):
+    logout(r)
+    return redirect('/login/')

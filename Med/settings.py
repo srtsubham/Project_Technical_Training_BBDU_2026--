eaf095bc997ca53c45395881b46ba2406b6874dd
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-05-09 10:31:24 +0530

    settings fixes v2.

--- Med/settings.py
+++ Med/settings.py
@@ -49,7 +49,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Med.wsgi.application'
 
-DATABASES = { #DATABASE 
+DATABASES = { #DATABA
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',

661e7c8b724e2041380591e5b4556fab0749d13e
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-05-11 20:47:08 +0530

    View URL fixing.

--- Shop/views.py
+++ Shop/views.py
@@ -10,7 +10,7 @@ def products(r):
     return render(r, 'products.html')
 
 def about(r):
-    return render(r, 'about.html')
+    return render(r, 'about.html') #hello
 
     if r.method == "POST":
      

0ce3d681ef150f7f499e1d8ee0746ffdcac8a15a
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-05-18 13:18:06 +0530

    Version control, glitch fixes 6.4

--- Shop/views.py
+++ Shop/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth import authenticate, login, logout
     def about(r):
         return render(r, 'about.html') 
 
-        if r.method == "POST": #V6.4
+        if r.method == "POST": #V6.5
         
 
             if m6 and m7:

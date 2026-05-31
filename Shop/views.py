88ebbb41e332a14945669bf4f6948700c68d625a
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-05-31 22:17:27 +0530

    Major update 7.0. Other glitch fixes.

--- Shop/views.py
+++ Shop/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth import authenticate, login, logout
     def about(r):
         return render(r, 'about.html') 
 
-        if r.method == "POST": #V6.9
+        if r.method == "POST": #V7.0
         
 
             if m6 and m7:

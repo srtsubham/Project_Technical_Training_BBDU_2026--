10612d82b5d224fb52b05842e98d30bd81c459bd
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-05-31 22:18:31 +0530

    Minor bug fixes

--- Shop/views.py
+++ Shop/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth import authenticate, login, logout
     def about(r):
         return render(r, 'about.html') 
 
-        if r.method == "POST": #V7.0
+        if r.method == "POST": #V7.1
         
 
             if m6 and m7:

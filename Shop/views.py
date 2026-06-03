2bc9532494c7d589b98573d04aa0bbde9222e7b3
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-06-03 22:28:53 +0530

    NAvbar glitch fix (Pixel leak fix)

--- Shop/views.py
+++ Shop/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth import authenticate, login, logout
     def about(r):
         return render(r, 'about.html') 
 
-        if r.method == "POST": #V7.7
+        if r.method == "POST": #V7.8
         
 
             if m6 and m7:

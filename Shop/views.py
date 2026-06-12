808ece20b6d650d7db028b3371c60a0771bb7003
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-06-12 22:16:20 +0530

    Update version 9.3

--- Shop/views.py
+++ Shop/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth import authenticate, login, logout
     def about(r):
         return render(r, 'about.html') 
 
-        if r.method == "POST": #V9.2
+        if r.method == "POST": #V9.3
         
 
             if m6 and m7:
